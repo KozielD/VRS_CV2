08abb3f6e09d52e4087099f23200d5064b284133
KozielD <mr.dakol@gmail.com>
2016-10-03 12:35:55 +0200

    Zadanie 1
    Zadanie 1

--- src/main.c
+++ src/main.c
@@ -92,10 +92,28 @@ void assert_failed(uint8_t* file, uint32_t line)
   /* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
 
-  /* Infinite loop */
-  while (1)
-  {
-  }
+
+
+	  /* Infinite loop */
+	  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+
+
+	  GPIOA->MODER |= (0b01)<<(5*2);
+	  GPIOA->OTYPER &=~ (0b1)<<(5);
+	  GPIOA->PUPDR |= (0b01)<<(5*2);
+	  GPIOA->OSPEEDR |= (0b11)<<(5*2);
+
+	  GPIOA->ODR |= (0b1)<<(5); //zapnutie pomocou ODR
+	  GPIOA->ODR &=~ (0b1)<<(5); //vypnutie pomocou ODR
+
+	  GPIOA->ODR ^= (0b1)<<(5); //prepinanie pomocou ODR
+
+
+	  while (1)
+	  {
+		  i++;
+	  }
+	  return 0;
 }
 #endif
 
